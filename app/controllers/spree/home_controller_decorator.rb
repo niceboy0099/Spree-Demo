bd0d4156e3b1795b6d2cdaaec105f61424ac02ee
gaurav <ggupta@grepruby.com>
2014-12-17 19:13:30 +0530

    removed duplicate products

---
+++ app/controllers/spree/home_controller_decorator.rb
@@ -0,0 +1,14 @@
+Spree::HomeController.class_eval do
+
+  def index
+    slider = Spree::Taxon.where(:name => 'Slider').first
+    @slider_products = slider.products.active.uniq if slider
+   
+    featured = Spree::Taxon.where(:name => 'Featured').first
+    @featured_products = featured.products.active.uniq if featured
+    
+    latest = Spree::Taxon.where(:name => 'Latest').first
+    @latest_products = latest.products.active.uniq if latest
+  end
+
+end
